de35cb89eeda389a94fe2c5327c3c723b1892721
Markus <marcoce05@gmail.com>
2023-03-23 02:24:35 -0400

    Webpack con React

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {saludo} from "./js/componetes";
+import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+saludo();
+saludo();
+
+ReactDOM.render(<h1>Hola Mund</h1>, document.getElementById('root'))
